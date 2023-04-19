d986d84f7664b7b823b20b8cc390c997bd09e177
deeksha01032023 <deekshapandey@cedcoss.com>
2023-04-19 15:59:34 +0530

    completed

---
+++ src/app/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+// defalut controller view
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+    //    default action
+    }
+}
